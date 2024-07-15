f6a0cb62f468e7a70bced4b2b0137a4e34956d12
AshSimmonds <ashsimmonds@gmail.com>
2024-07-15 22:39:51 +1000

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -7,20 +7,20 @@ export default defineStackbitConfig({
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
-      contentDirs: ["routes"],
+      contentDirs: ["content"],
       models: [
         {
           name: "Page",
           type: "page",
           urlPath: "/{slug}",
-          filePath: "routes/{slug}.json",
+          filePath: "content/pages/{slug}.json",
           fields: [{ name: "title", type: "string", required: true }]
         }
       ],
       assetsConfig: {
         referenceType: "static",
         staticDir: "public",
-        uploadDir: "assets/images",
+        uploadDir: "images",
         publicPath: "/"
       }
     })
